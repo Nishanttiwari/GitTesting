11483e013ac5e68c8dd8782bfce7947dda9f787b
Mayank Yadav <yadavm4@aviva.co.uk>
2012-09-21 13:50:03 +0100

    Added file DemoTest.cs

---
+++ Guestbook.Tests/Controllers/DemoTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Guestbook.Tests.Controllers
+{
+    /// <summary>
+    /// Added file just to learn git merge command
+    /// </summary>
+    [TestClass]
+    public class DemoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
